1212e9ca276f40298e5cc6a8bbf5e8a5d2af80f6
Atsushi Kokubu <akokubu@gmail.com>
2026-05-10 20:38:18 +0900

    create hello and goodby functions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,15 @@
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
+    printt_hello();
+    print_goobye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
 }
+
+fn print_gooby() {
+    println!("Goodbye, world");
+}
+
