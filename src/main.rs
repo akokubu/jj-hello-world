c1bf5a45d27ec44daff743114277a0dbc86f9ed0
Atsushi Kokubu <akokubu@gmail.com>
2026-05-14 16:52:03 +0900

    refactor printing

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@
 /// This is the best implementation of this program to ever exist.
 
 fn main() {
-    printt_hello();
-    print_goobye();
+    print("Hello, world!");
+    print("Goodbye, world!");
 }
 
-fn print_hello() {
-    println!("Hello, world!");
-}
-
-fn print_gooby() {
-    println!("Goodbye, world");
+// a function that prints a message
+fn print(m: &str) {
+    println!("{m}")
 }
 
